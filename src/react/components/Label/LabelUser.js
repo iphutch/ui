b139c0f16bd75a5c7d92e70e4ed5fc763c926d29
Eddie Ramirez <djedir@gmail.com>
2018-06-15 09:57:38 -0700

    Updated proxy

---
+++ src/react/components/Label/LabelUser.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Label from "./Label";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faUser from "@fortawesome/fontawesome-free-solid/faUser";
+
+const LabelUser = ({ user }) => {
+  return (
+    <Label
+      icon={<FontAwesomeIcon icon={faUser} />}
+      text={user.username}
+      title={user.username}
+    />
+  );
+};
+
+LabelUser.propTypes = {
+  user: PropTypes.object
+};
+
+export default LabelUser;
