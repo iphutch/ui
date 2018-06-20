e1b60c97df083bd4e421236ceaf2d38509f5222b
Eddie Ramirez <djedir@gmail.com>
2018-06-20 00:58:03 -0700

    Cleanup unused components

--- src/react/containers/FilterBar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FilterBar = ({ children }) => {
-  return <div className="filter-bar">{children}</div>;
-};
-
-export default FilterBar;
--- src/react/containers/index.js
+++ src/react/containers/index.js
@@ -1,4 +1,3 @@
 import App from "./App";
-import MainManPages from "./MainManpages";
 import MainCommands from "./MainCommands";
-export { App, MainCommands, MainManPages };
+export { App, MainCommands };
