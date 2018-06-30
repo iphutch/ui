af981cbdb798dac2fc469bf24c647f5fe947db76
Eddie Ramirez <djedi.r@gmail.com>
2018-06-29 22:38:23 -0700

    Returned view manual page button (#14)

--- src/react/components/Program/ProgramTitle.js
+++ src/react/components/Program/ProgramTitle.js
@@ -1,8 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const ProgramTitle = ({ name, platformName, cliName }) => {
-  return <h2>What does {name} do?</h2>;
+  return (
+    <h2>
+      What does {name} do?{" "}
+      <Button
+        color="success"
+        size="sm"
+        tag={Link}
+        to={`/manpages/${platformName}/${name}`}
+      >
+        view manual page
+      </Button>
+    </h2>
+  );
 };
 
 ProgramTitle.propTypes = {
